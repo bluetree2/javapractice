4a4844a3d0189b22cb327b3ff0875c0ac5e3843a
bluetree2 <kmsid77@gmail.com>
2025-04-28 14:31:20 +0900

    정수형

---
+++ src/ch02/sec02/LongExample.java
@@ -0,0 +1,15 @@
+package ch02.sec02;
+
+public class LongExample {
+    public static void main(String[] args) {
+        long var1 = 10;
+        long var2 = 20L;
+//        long var3 = 1000000000000000000; // 컴파일러는 int로 간주해서 오류
+//        undersocer(underbar)로 자릿수 표시 가능
+        long var4 = 1_000_000_000_000_000_000L;
+
+        System.out.println(var1);
+        System.out.println(var2);
+        System.out.println(var4);
+    }
+}
