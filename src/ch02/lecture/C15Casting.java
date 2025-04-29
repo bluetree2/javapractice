804b7294b385705d8b713b1225a295ac290351ae
bluetree2 <kmsid77@gmail.com>
2025-04-29 10:11:04 +0900

    형 강제 변환

---
+++ src/ch02/lecture/C15Casting.java
@@ -0,0 +1,32 @@
+package ch02.lecture;
+
+public class C15Casting {
+    public static void main(String[] args) {
+        // 작은 타입 -> 큰 타입 (문제 없음)
+
+        //큰 타입  -> 작은 타입 (문제가 있을 수 있음, 매우 조심해야함)
+
+        byte a;
+        short b;
+        b = 100;
+        System.out.println("b = " + b); // 100
+
+        // 기본적으로 안됨, 강제로 해야함 (type casting)
+        // 강제 형 변환
+        a = (byte) b;
+        System.out.println("a = " + a);
+
+        b = 128;
+        a = (byte) b;
+        // 오버플로우 발생
+        System.out.println("a = " + a);
+
+        b = 987;
+        a = (byte) b;
+        System.out.println("b = " + b);
+        System.out.println("a = " + a);
+        b = a;
+        System.out.println("b = " + b);
+
+    }
+}
