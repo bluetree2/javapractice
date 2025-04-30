c44186b6086009c94850ee24296ca6337e370e3a
bluetree2 <kmsid77@gmail.com>
2025-04-30 10:18:45 +0900

    삼항 연산자

---
+++ src/ch03/lecture/C14Ternary.java
@@ -0,0 +1,19 @@
+package ch03.lecture;
+
+public class C14Ternary {
+    public static void main(String[] args) {
+        // 삼항연산자(ternary)
+        // 조건연산자(conditional)
+
+        //  항1 ? 항2 : 항3
+        // 항1이 true이면 항2를 사용
+        // 항1이 false이면 항3을 사용
+
+        int r1 = true ? 3 : 5;
+        int r2 = false ? 3 : 5;
+
+        System.out.println(r1);
+        System.out.println(r2);
+        
+    }
+}
