f3d1a041cef17a4631f1b8d5eb3a7d4448b31b28
bluetree2 <kmsid77@gmail.com>
2025-05-08 17:48:49 +0900

    생성자

---
+++ src/ch06/lecture/p05constructor/App07.java
@@ -0,0 +1,55 @@
+package ch06.lecture.p05constructor;
+
+public class App07 {
+
+}
+
+class Myclass07 {
+    String name;
+    int age;
+    String email;
+    boolean married;
+
+    public Myclass07() {
+
+//        this.name = "홍길동";
+//        this.age = 1;
+//        this.email = "없음";
+//        this.married = false;
+
+        this("홍길동", 1, "없음", false);
+
+    }
+
+    public Myclass07(String name) {
+//        this.name = name;
+//        this.age = 1;
+//        this.email = "없음";
+//        this.married = false;
+        this(name, 1, "없음", false);
+    }
+
+    public Myclass07(String name, int age) {
+//        this.name = name;
+//        this.age = age;
+//        this.email = "없음";
+//        this.married = false;
+        this(name, age, "없음", false);
+    }
+
+    public Myclass07(String name, int age, String email) {
+//        this.name = name;
+//        this.age = age;
+//        this.email = email;
+//        this.married = false;
+        this(name, age, email, false);
+    }
+
+    public Myclass07(String name, int age, String email, boolean married) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.married = married;
+//        this(name, age, email, married);
+    }
+}
