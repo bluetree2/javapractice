53a5c8f60f5896e2c73af7d11b2ac0124135fe6d
bluetree2 <kmsid77@gmail.com>
2025-05-09 14:46:22 +0900

    public, private, static

--- src/ch06/lecture/p09access/sub1/App05.java
+++ src/ch06/lecture/p09access/sub1/App05.java
@@ -2,6 +2,6 @@ package ch06.lecture.p09access.sub1;
 
 public class App05 {
     public static void main(String[] args) {
-        MyClass05 a = new MyClass05();
+//        MyClass05 a = new MyClass05();
     }
 }
