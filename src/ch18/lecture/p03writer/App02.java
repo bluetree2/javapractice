c7dc7f937c6ac82b8bb2673b1085977ba49f5b54
bluetree2 <kmsid77@gmail.com>
2025-05-26 12:22:23 +0900

    writer

--- src/ch18/lecture/p03writer/App02.java
+++ src/ch18/lecture/p03writer/App02.java
@@ -1,11 +1,33 @@
 package ch18.lecture.p03writer;
 
+import ch18.sec02.exam01.WriteExample;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
 public class App02 {
-    public static void main(String[] args) {
-        //inputStream, outpurStream ㅇ로
+    public static void main(String[] args) throws IOException {
+        // byte 단위
+        //inputStream, outputStream 으로
         // answk(ascii 코드 외) 를 읽고 쓰는게 너~~무 불편하다
 
         // 문자(charactoer)단위
         // reader, writer 가 추가됨
+
+        // writer : 문자 단위 출력 스트림
+
+        String fileName = "C:/Temp/writer02.txt";
+        try (Writer writer = new FileWriter(fileName)) {
+            writer.write('a');
+            writer.write(97);
+            writer.write('걸');
+            writer.write(44152);
+            writer.flush();
+        }
+
+        System.out.println("파일 쓰기 완료");
+
+
     }
 }
